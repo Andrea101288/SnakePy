7a2fa9d0f6de928a8328da20437e938fcecb3f67
Andrea Mancini <andrea101288@hotmail.it>
2020-04-25 13:33:46 +0200

    remove useless comments

--- settings/GameSetting.py
+++ settings/GameSetting.py
@@ -9,43 +9,35 @@ class GameSetting:
 
     # Move funcions
     def go_up(self):
-        # set the direction to up
         self.gameStyle.head.direction = "up"
 
     def go_down(self):
-        # set the direction to down
         self.gameStyle.head.direction = "down"
 
     def go_left(self):
-        # set the direction to left
         self.gameStyle.head.direction = "left"
 
     def go_right(self):
-        # set the direction to right
         self.gameStyle.head.direction = "right"
 
     def move(self):
 
+         # get head coordinates and increase it of 20 pxls
         if self.gameStyle.head.direction == "up":
-            # get head y coordinate
+           
             y = self.gameStyle.head.ycor()
-            # increase head y coordinates of 20 pxls
             self.gameStyle.head.sety(y + 20)
             
-        if self.gameStyle.head.direction == "down":
-            # get head y coordinate
-            y = self.gameStyle.head.ycor()
-            # decrease head y coordinates of 20 pxls
+        if self.gameStyle.head.direction == "down":          
+            y = self.gameStyle.head.ycor()          
             self.gameStyle.head.sety(y - 20)
 
         if self.gameStyle.head.direction == "left":
-            # get head x coordinate
+         
             x = self.gameStyle.head.xcor()
-            # increase head x coordinates of 20 pxls
+           
             self.gameStyle.head.setx(x + 20)
 
-        if self.gameStyle.head.direction == "right":
-            # get head x coordinate
-            x = self.gameStyle.head.xcor()
-            # decrease head x coordinates of 20 pxls
+        if self.gameStyle.head.direction == "right":          
+            x = self.gameStyle.head.xcor()          
             self.gameStyle.head.setx(x - 20)
--- styles/GameStyle.py
+++ styles/GameStyle.py
@@ -21,47 +21,31 @@ class GameStyle:
     # Turns turtle animation on/off and set delay for update drawings.
     wn.tracer(0)
 
-    # Snake Head creation
+
     head = turtle.Turtle()
-    # set head initial speed
     head.speed(0)
-    # set head shape
     head.shape("square")
-    # set head color
     head.color("black")
-    # Sets the current pen state to penup, not drawing it
     head.penup()
-    # initialize the head ih 0,0 coordinates
     head.goto(0,0)
-    # Intialize the movement to stop
     head.direction = "stop"
 
-    # Snake Food creation
+
     food = turtle.Turtle()
-    # Initialize food speed to 0
     food.speed(0)
-    # set food Shape
     food.shape("circle")
-    # set food color
     food.color("red")
-    # set the food to penUp, not drawing it
     food.penup()
-    # set iniitial coordinates for food
     food.goto(0,100)
 
-    # Snake Head creation
+
     mypen = turtle.Turtle()
-    # set head color
     mypen.color("white")
 
-    # define segments array to add everytime we get food
     segments = []    
 
-    #eating soundtrack
     eating_sound = r"./sounds/pureskelu.mp3"
-
-    # set initial score 
+ 
     score = 0
 
-    # variable gameOver initialized to False
     game_over = False
